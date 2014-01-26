2e6b5a70048f9e2608200294cc9a4fc7106355f2
John Terragnoli <C16John.Terragnoli@usafa.edu>
2014-01-25 23:59:21 -0700

    8 bit update

--- Lab1_Terragnoli.vhd
+++ Lab1_Terragnoli.vhd
@@ -19,6 +19,8 @@
 --
 ----------------------------------------------------------------------------------
 library IEEE;
+--sets up a library that sets up signed and unsigned bits.  
+use IEEE.NUMERIC_STD.ALL;
 use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
@@ -31,42 +33,50 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Lab1_Terragnoli is
-    Port ( A : in  STD_LOGIC;
-           B : in  STD_LOGIC;
-           C : in  STD_LOGIC;
-           X : out  STD_LOGIC;
-           Y : out  STD_LOGIC;
-           Z : out  STD_LOGIC);
+--STD_LOGIC_VECTOR groups all of the bits together like a number 
+    Port ( A:in STD_Logic_vector (7 DOWNTO 0);
+	 Z: out STD_LOGIC_VECTOr(7 DOWNTO 0));
+	 
+	 
+	 --A : in  STD_LOGIC;
+           --B : in  STD_LOGIC;
+           --C : in  STD_LOGIC;
+           --X : out  STD_LOGIC;
+           --Y : out  STD_LOGIC;
+           --Z : out  STD_LOGIC);
 end Lab1_Terragnoli;
 
 architecture Behavioral of Lab1_Terragnoli is
 --signal declarations: 
-signal A_NOT: STD_LOGIC;
-signal B_NOT: STD_LOGIC;
-signal C_NOT: STD_LOGIC;
-signal H: STD_LOGIC;
-signal I: STD_LOGIC;
-signal J: STD_LOGIC;
-signal K: STD_LOGIC;
-signal L: STD_LOGIC;
+--signal A_NOT: STD_LOGIC;
+--signal B_NOT: STD_LOGIC;
+--signal C_NOT: STD_LOGIC;
+--signal H: STD_LOGIC;
+--signal I: STD_LOGIC;
+--signal J: STD_LOGIC;
+--signal K: STD_LOGIC;
+--signal L: STD_LOGIC;
 
 begin
-A_NOT <= not A;
-B_NOT <= not B;
-C_NOT <= not C;
+--this is for the 8 bit converter
+Z <= STD_LOGIC_VECTOR(UNSIGNED(not A)+1); 
 
-H<= A and B_NOT and C_NOT;
-I<= A_NOT and B;
-J<=A_NOT and C;
-
-X<=H or I or J; 
-
-K<=B_NOT and C;
-L<=B and C_NOT;
-
-Y<= K or L;
-
-Z<= C; 
+--A_NOT <= not A;
+--B_NOT <= not B;
+--C_NOT <= not C;
+--
+--H<= A and B_NOT and C_NOT;
+--I<= A_NOT and B;
+--J<=A_NOT and C;
+--
+--X<=H or I or J; 
+--
+--K<=B_NOT and C;
+--L<=B and C_NOT;
+--
+--Y<= K or L;
+--
+--Z<= C; 
 
 
 end Behavioral;
